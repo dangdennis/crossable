754a3d60d335b97f132cd76aa30099d80303b75d
Dennis Dang <dangggdennis@gmail.com>
2020-09-12 10:36:33 -0400

    refactor bomb_command to use with condition

--- crossing-bot/lib/crossing/commands/bomb_command.ex
+++ crossing-bot/lib/crossing/commands/bomb_command.ex
@@ -2,20 +2,17 @@ defmodule Crossing.Commands.Bomb do
   alias Crossing.Users
 
   def invoke(msg) do
-    case Users.get_user_by_discord_id(msg.author.id |> Integer.to_string()) do
-      nil ->
+    with {:ok, user} <- Users.get_user_by_discord_id(msg.author.id |> Integer.to_string()),
+         {:ok, _user} <- Users.delete_user(%Crossing.Users.User{id: user.id}) do
+      Nostrum.Api.create_message!(msg.channel_id, """
+      Your account and data has been thoroughly deleted. Like as in, I don't know you know anymore.
+      """)
+    else
+      {:error, reason} ->
+        IO.inspect(reason)
+
+      _ ->
         nil
-
-      user ->
-        case Users.delete_user(%Crossing.Users.User{id: user.id}) do
-          {:ok, _user} ->
-            Nostrum.Api.create_message!(msg.channel_id, """
-            Your account and data has been thoroughly deleted. Like as in, I don't know you know anymore.
-            """)
-
-          _ ->
-            nil
-        end
     end
   end
 end
