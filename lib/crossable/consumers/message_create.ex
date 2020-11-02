6338ef3a5a12f1fc10fb4e38f0af6f591a76c79c
Dennis Dang <dangggdennis@gmail.com>
2020-11-01 21:55:01 -0800

    update user create message

--- lib/crossable/consumers/message_create.ex
+++ lib/crossable/consumers/message_create.ex
@@ -16,20 +16,15 @@ defmodule Crossable.Consumer.MessageCreate do
                  discord_user_id: msg.author.id |> Integer.to_string(),
                  avatar: %{}
                }) do
-            {:ok, user} ->
+            {:ok, _user} ->
               Nostrum.Api.create_message(
                 msg.channel_id,
                 """
-                Welcome to Health Crossable! You're all set!
+                Welcome to Crossable! You're all set!
 
-                Commonly used commands:
-                !raid - learn what raid is happening this week
-                !join - to join the week's raid
-                !attack - confirm that you've completed your daily task, and deal some damage to the raid boss!
-                !help - get a list of all available commands
-
-                Real serious commands:
-                !bomb - deletes all your data
+                Commands:
+                !mindful - stay mindful and check-in for your habit progress!
+                !wallet - see your token balance.
                 """
               )
 
