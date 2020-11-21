1d3c05df9ce89b67ac96ad878bc73ee97c694ebf
Dennis Dang <dangggdennis@gmail.com>
2020-11-21 15:35:50 -0500

    add upperbound to reminder job

--- lib/crossable/time.ex
+++ lib/crossable/time.ex
@@ -3,27 +3,32 @@ defmodule Crossable.Time do
   Crossable time helpers.
   """
 
-  def run_at_time(now_time, run_time, callback) do
-    case Time.compare(now_time, run_time) do
-      :lt ->
-        IO.puts("lt")
-        IO.inspect(now_time)
-        IO.inspect(run_time)
+  @doc """
+  Runs the callback only if the given time is within the same hour as the runtime
+  """
+  def run_once_within_hour(current_time, run_time, callback) do
+    # lower bound: current time is greater than or equal to the desired runtime
+    # upper bound: current time is less than to 1 hour after the desired runtime
+    case {Time.compare(current_time, run_time),
+          Time.compare(current_time, Time.add(run_time, 60 * 59 + 59) |> Time.truncate(:second))} do
+      {:eq, :lt} ->
+        callback.()
+        true
+
+      {:eq, :eq} ->
+        callback.()
+        true
+
+      {:gt, :lt} ->
+        callback.()
+        true
+
+      {:gt, :eq} ->
+        callback.()
+        true
+
+      {_, _} ->
         false
-
-      :eq ->
-        IO.puts("eq")
-        IO.inspect(now_time)
-        IO.inspect(run_time)
-        callback.()
-        true
-
-      :gt ->
-        IO.puts("gt")
-        IO.inspect(now_time)
-        IO.inspect(run_time)
-        callback.()
-        true
     end
   end
 end
--- test/crossable/time_test.exs
+++ test/crossable/time_test.exs
@@ -1,27 +1,25 @@
 defmodule Crossable.TimeTest do
   use Crossable.DataCase
 
-  describe "Crossable.Time.run_at_time/3" do
-    test "should execute callback if time is gt or eq to desired time" do
+  describe "Crossable.Time.run_once_within_hour/3" do
+    test "should execute callback if time is gt or eq to desired time, and only within that hour" do
       assert true ==
-               Crossable.Time.run_at_time(~T[18:00:00], ~T[18:00:00], fn ->
-                 nil
-               end)
+               Crossable.Time.run_once_within_hour(~T[18:00:00], ~T[18:00:00], fn -> nil end)
 
       assert true ==
-               Crossable.Time.run_at_time(~T[18:00:01], ~T[18:00:00], fn ->
-                 nil
-               end)
+               Crossable.Time.run_once_within_hour(~T[18:00:01], ~T[18:00:00], fn -> nil end)
+
+      assert true ==
+               Crossable.Time.run_once_within_hour(~T[18:59:58], ~T[18:00:00], fn -> nil end)
 
       assert false ==
-               Crossable.Time.run_at_time(~T[17:00:00], ~T[18:00:00], fn ->
-                 nil
-               end)
+               Crossable.Time.run_once_within_hour(~T[19:00:00], ~T[18:00:00], fn -> nil end)
 
       assert false ==
-               Crossable.Time.run_at_time(~T[17:59:59], ~T[18:00:00], fn ->
-                 nil
-               end)
+               Crossable.Time.run_once_within_hour(~T[17:00:00], ~T[18:00:00], fn -> nil end)
+
+      assert false ==
+               Crossable.Time.run_once_within_hour(~T[17:59:59], ~T[18:00:00], fn -> nil end)
     end
   end
 end
